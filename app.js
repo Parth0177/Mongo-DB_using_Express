6bb84cafa1c5b352cef0edf7d9310b9343d59ac0
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-25 01:27:14 +0530

    Add chat model with schema definition for messages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app= express();
 const PORT=5500;
 const path = require('path');
 const mongoose = require('mongoose');
+const Chat = require("./models/chat.js");
 
 main().then(()=>{
   console.log('connection Successful');
@@ -12,6 +13,18 @@ main().then(()=>{
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/WhatsApp");
 };
+
+let chat1 = new Chat({
+  from:"Parth",
+  to:"Gungun",
+  msg:"Hey! Cutie",
+  created_at: new Date()
+});
+chat1.save().then((res)=>{
+  console.log(res);
+  
+}).catch((err)=> console.log(err)
+)
 
 app.set("views", path.join(__dirname,"views"));
 app.set('view engine','ejs');
