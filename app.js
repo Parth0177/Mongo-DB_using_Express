0bebe26a3b45b0d00ae101187ee032366df7eba1
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-25 00:56:24 +0530

    installed

---
+++ app.js
@@ -0,0 +1,12 @@
+const express= require('express');
+const app= express();
+const PORT=5500;
+const path = require('path');
+const mongoose = require('mongoose');
+
+async 
+
+app.listen("PORT", (req,res)=>{
+  console.log(`Server is running on PORT ${PORT}`);
+  
+})
