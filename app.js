65c71f7682df88d21bd1fcab6ec9945ad2f66092
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-25 01:01:36 +0530

    Created 1st route

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ const PORT=5500;
 const path = require('path');
 const mongoose = require('mongoose');
 
-async 
+main().then(()=>{
+  console.log('connection Successful');
+  
+}).catch((err)=> console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+
+app.set("views", path.join(__dirname,"views"));
+app.set('view engine','ejs');
+
+
+app.get('/',(req,res)=>{
+  res.send("Wroking Root");
+})
 
 app.listen("PORT", (req,res)=>{
   console.log(`Server is running on PORT ${PORT}`);
